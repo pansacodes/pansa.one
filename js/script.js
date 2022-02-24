e2b635dcc1313c3c870e4b7d1da316857b2e2639
saurav-pandey <sauravppandey@gmail.com>
2022-02-25 02:13:24 +0530

    Added projects

--- js/script.js
+++ js/script.js
@@ -7,6 +7,5 @@ function changeText() {
     callElement("subheading").innerHTML += " I enjoy creating things that live on the internet, be it websites, apps, or anything in between. I've managed to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work. I'm fueled by coffee & the lyrical magic of music";
 }
 function copyText() {
-    navigator.clipboard.writeText("");
-
+    navigator.clipboard.writeText("sauravppandey@gmail.com");
 }
