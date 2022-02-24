504f9b52459c89893edd77ce837c99022cb519ad
saurav-pandey <sauravppandey@gmail.com>
2022-02-25 02:26:01 +0530

    Fotter Links

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function callElement(pass) {
 
 function changeText() {
     callElement("readmore").style.display = `none`;
-    callElement("subheading").innerHTML += " I enjoy creating things that live on the internet, be it websites, apps, or anything in between. I've managed to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work. I'm fueled by coffee & the lyrical magic of music";
+    callElement("subheading").innerHTML += " I enjoy creating things that live on the internet, be it websites, apps, or anything in between. I've managed to gain a decent amount of experience and valuable knowledge from the different kinds of projects. I'm fueled by coffee & driven by the lyrical magic of music";
 }
 function copyText() {
     navigator.clipboard.writeText("sauravppandey@gmail.com");
