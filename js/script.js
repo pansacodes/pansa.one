bfe714bca98c2aa162e700b3c8bfe867662b78d2
saurav-pandey <sauravppandey@gmail.com>
2022-02-24 17:40:03 +0530

    Major changes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function callElement(pass) {
+    return document.getElementById(pass);
+}
+
+function changeText() {
+    callElement("readmore").style.display = `none`;
+    callElement("subheading").innerHTML += " I enjoy creating things that live on the internet, be it websites, apps, or anything in between. I've managed to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work. I'm fueled by coffee & the lyrical magic of music";
+}
