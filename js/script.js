494982d8f7ac65ced12f02db2519aba8a459a313
saurav-pandey <sauravppandey@gmail.com>
2022-02-27 05:24:13 +0530

    Image blink

--- js/script.js
+++ js/script.js
@@ -21,12 +21,15 @@ function jagMag() {
         return [callElement("cosmos").style.opacity = a, callElement("offers").style.opacity = b, callElement("tr").style.opacity = c, callElement("wishlist").style.opacity = d, callElement("vernacular").style.opacity = e];
     }
 
-    function callingCards(cardName, a, b, c, d, e) {
+    function callingCards(cardName, a, b, c, d, e, imgRoute) {
         cardName.addEventListener("mouseenter", function (event) {
             callAllElements(a, b, c, d, e);
+            document.getElementById("supporting-image").src = `${imgRoute}`;
+
         })
         cardName.addEventListener("mouseleave", function (event) {
             callAllElements(1, 1, 1, 1, 1);
+            document.getElementById("supporting-image").src = ``; //path left empty
         })
     }
     var firstCard = callElement("cosmos");
@@ -35,11 +38,12 @@ function jagMag() {
     var fourthCard = callElement("wishlist");
     var fifthCard = callElement("vernacular");
 
-    callingCards(firstCard, 1, 0.5, 0.5, 0.5, 0.5);
-    callingCards(secondCard, 0.5, 1, 0.5, 0.5, 0.5);
-    callingCards(thirdCard, 0.5, 0.5, 1, 0.5, 0.5);
-    callingCards(fourthCard, 0.5, 0.5, 0.5, 1, 0.5);
-    callingCards(fifthCard, 0.5, 0.5, 0.5, 0.5, 1);
+    callingCards(firstCard, 1, 0.5, 0.5, 0.5, 0.5, "./resources/projects/Cosmos-cover.png");
+    callingCards(secondCard, 0.5, 1, 0.5, 0.5, 0.5, "./resources/projects/Refund.png");
+    callingCards(thirdCard, 0.5, 0.5, 1, 0.5, 0.5, "./resources/projects/s.jpg");
+    callingCards(fourthCard, 0.5, 0.5, 0.5, 1, 0.5, "./resources/projects/Refund.png");
+    callingCards(fifthCard, 0.5, 0.5, 0.5, 0.5, 1, "./resources/projects/Refund.png");
 }
 
 jagMag();
+
