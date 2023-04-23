98f4ca2b873a50c6e0513f212266e0e1ec5ae3f7
panzacodes <sauravppandey@gmail.com>
2023-04-23 12:51:14 +0530

    few basic udates - footer, header

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function callElement(pass) {
 function changeText() {
 	callElement('readmore').style.display = `none`;
 	callElement('subheading').innerHTML +=
-		" I enjoy creating things that live on the internet websites apps & anything in between. I've managed to gain a decent amount of experience and valuable knowledge from different kinds of projects. I'm fueled by coffee & driven by the lyrical magic of music";
+		" I enjoy creating things that live on the internet, websites apps & anything in between. I've managed to gain a decent amount of experience and valuable knowledge from different kinds of projects. I'm fueled by coffee & driven by the lyrical magic of music.";
 }
 function copyText() {
 	navigator.clipboard.writeText('sauravppandey@gmail.com');
